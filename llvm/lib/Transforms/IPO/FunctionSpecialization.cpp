7ab399cdd709e40ad1c6d38de191761bad9dc24e
bababuck <buchner.ryan@gmail.com>
2025-09-29 18:44:09 -0700

    [FnSpecialization] (2/6) Avoid creating standalone specializations when only ever part of a chain
    Will get specialized as part of the chain if the chain scores well enough.

--- llvm/lib/Transforms/IPO/FunctionSpecialization.cpp
+++ llvm/lib/Transforms/IPO/FunctionSpecialization.cpp
@@ -793,6 +793,10 @@ bool FunctionSpecializer::run() {
   // specialization budget, which is derived from maximum number of
   // specializations per specialization candidate function.
   auto CompareScore = [&AllSpecs](unsigned I, unsigned J) {
+    if (AllSpecs[J].AllChains)
+      return true;
+    if (AllSpecs[I].AllChains)
+      return false;
     if (AllSpecs[I].Score != AllSpecs[J].Score)
       return AllSpecs[I].Score > AllSpecs[J].Score;
     return I > J;
@@ -1225,6 +1229,7 @@ bool FunctionSpecializer::findSpecializations(
 
       if (CS.getFunction() == F && !Spec.CallSites[0].Parent) {
         Spec.CallSites.clear();
+        Spec.AllChains = true;
       }
       UniqueSpecs[S] = Index;
 
